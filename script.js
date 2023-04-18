8274b6867e88d19c14ee900f62bfdce674f013f5
Lukáš Lešák <lukas.kalna@seznam.cz>
2023-04-18 18:18:56 +0200

    Theme

---
+++ script.js
@@ -0,0 +1,11 @@
+/***** Theme change *****/
+const btn = document.querySelector(".btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+  } else {
+    document.body.classList.toggle("dark-theme");
+  }
+});
